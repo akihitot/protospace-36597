2887fd18a23c91fcefacfc73861be8b7a6fb6b62
akihitot <atokimatu2@gmail.com>
2021-08-17 13:46:51 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root to: "prototypes#index" #ルートパスにアクセスしたらprototypesコントローラーのindexアクションが呼ばれる
+  # root to: "prototypes#index" #ルートパスにアクセスしたらprototypesコントローラーのindexアクションが呼ばれる
+  devise_for :users
+  root to: 'prototypes#index'
+  
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
+  
+  resources :prototypes
+  resources :users, only: :show
 end
